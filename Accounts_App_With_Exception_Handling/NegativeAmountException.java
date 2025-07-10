29117bc6bca924578d5d1943fed535a78a06d6a7
Niket-Shah28 <113494381+Niket-Shah28@users.noreply.github.com>
2025-07-10 10:40:17 +0530

    Delete Accounts_App_With_Exception_Handling directory

--- Accounts_App_With_Exception_Handling/NegativeAmountException.java
+++
@@ -1,9 +0,0 @@
-package com.aurionpro.assignment;
-
-public class NegativeAmountException extends RuntimeException{
-	
-	@Override
-	public String getMessage() {
-		return "Amount to be deposited / credited must be greather than Zero";
-	}
-}
